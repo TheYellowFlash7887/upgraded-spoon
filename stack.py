a7dc59155a0268d0bee49de6edf289cfeb748e11
Jmcg <mcgrathjon7887@gmail.com>
2021-12-16 14:16:19 +0000

    added basic functions for peak list push and remove

--- stack.py
+++ stack.py
@@ -1,11 +1,35 @@
 class stack():
     def __init__(self):
-        contents=[]
-    def add(self):
-        pass
+        self.contents = []
+
+    def push(self, item):
+        print('weird {0}'.format(item))
+        if item is not None:
+            self.contents.insert(0, item)
+        else:
+            return 'Cant push empty item'
+
     def remove(self):
-        pass
+        return self.contents.pop(0)
+
     def size(self):
+        return self.contents.count()
+
+    # return the top item of the stack
+    def peak(self):
+        return self.contents[0]
+
+    def list_Items(self):
+        return self.contents.copy()
+
+    def empty(self):
         pass
 
+
+if __name__ == '__main__':
+    testStak = stack()
+    testStak.push(0)
+    testStak.push('test')
+    testStak.push()
+    print(testStak.list_Items())
 
